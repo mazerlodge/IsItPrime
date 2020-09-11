f3c38e96feaef25a29663a19ba551843739010f6
mazerlodge <mazerlodge@hotmail.com>
2020-09-10 21:21:18 -0500

    Initial version

---
+++ main.c
@@ -0,0 +1,36 @@
+//
+//  main.c
+//  IsItPrime
+//
+//  Created by Mazerlodge on 9/10/20.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, const char * argv[]) {
+	
+	if (argc < 2) {
+		printf("Usage: One integer argument, e.g. isItPrime 58811");
+		return -1;
+		
+	}
+	
+	if ((argc == 3) && (strncmp(argv[2], "42", 2) == 0))
+		printf("Build Path: %s\n", argv[0]);
+	
+
+	int targetVal = atoi(argv[1]);
+
+	for (int x=2; x<targetVal; x++) {
+		int remainder = targetVal % x;
+		
+		if (remainder == 0)
+			printf("%d evenly divisible by %d\n", targetVal, x);
+		
+	}
+	
+	
+	return 0;
+}
